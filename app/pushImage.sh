4507e66ffcc77daa3fc9e6657f6236b3a127e026
Nikita Koshelev <39601787+HuKuTa94@users.noreply.github.com>
2021-10-11 22:02:05 +0500

    docker builds image now successfully

--- app/pushImage.sh
+++ app/pushImage.sh
@@ -2,13 +2,12 @@
 set -e
 
 imageTag=$1
-if [ -z "$1"]
+if [ -z "$1" ]
   then
-    echo No image tag provided. Latest will be used
+    echo No image tag provided. latest will be used
     imageTag=latest
 fi
-
-repositoryName=bob
+repositoryName=app
 imageFullName=$repositoryName:$imageTag
 
 echo [App STARTING] pushing image $imageFullName...
@@ -16,4 +15,4 @@ echo [App STARTING] pushing image $imageFullName...
 echo [App] pushing image...
 docker push $imageFullName
 
-echo [App FINISHED] image $imageFullName has been pushed
+echo [App FINISHED] image $imageFullName pushed
